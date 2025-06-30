e2baf21be438b64745cc667bfb6d7b2b0609a583
Mr.Umer <umer.shahzad.dev@gmail.com>
2025-06-30 13:48:31 +0500

    connect database

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
 const app = express();
 
 dotenv.config();
@@ -13,10 +14,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 const PORT = process.env.PORT || 4001;
 
+// connect to db
+connectDB();
+
 app.get("/", (req,res)=>{
     res.send("Server running");
 })
 
 app.listen(PORT, ()=>{
-    console.log(`Server is running on PORT ${PORT}`);
+    console.log(`✅ Server is running on PORT ${PORT}`);
 })
